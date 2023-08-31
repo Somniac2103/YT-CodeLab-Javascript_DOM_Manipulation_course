3390c42074c475d833dea91936fde63eb9672459
Somniac2103 <salmon.barry1@gmail.com>
2023-08-31 21:07:49 +0800

    Completed Project 2 and 3 of the course on DOM manipulation

--- Project2/project-2.js
+++ Project2/project-2.js
@@ -1,29 +1,23 @@
-//Variables
+//variables
 
-let btn = document.querySelector('#new-quote');
-let quote = document.querySelector('.quote');
-let person = document.querySelector('.person');
+let openBtn = document.getElementById('open-btn');
+let modalContainer = document.getElementById('modal-container');
+let closeBtn = document.getElementById('close-btn');
 
-const quotes = [{
-  quote: '“Be yourself; everyone else is already taken.”',
-  person: ' Oscar Wilde'
-},{
-  quote: `“Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.”`,
-  person: ' Albert Einstein'
-},{
-  quote: '“So many books, so little time.”',
-  person: ' Frank Zappa'
-},{
-  quote: `“Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.”`,
-  person: ' Bernard M. Baruch'
-}];
+//Event Listeners
 
-btn.addEventListener('click', function(){
+openBtn.addEventListener('click', function(){
 
-  let random = Math.floor(Math.random() * quotes.length);
-
-  quote.innerText = quotes[random].quote;
-  person.innerText = quotes[random].person;
+  modalContainer.style.display = 'block';
 });
 
+closeBtn.addEventListener('click', function(){
 
+  modalContainer.style.display = 'none';
+});
+
+window.addEventListener('click', function (e){
+  if(e.target === modalContainer){
+    modalContainer.style.display = 'none';
+  }
+});
